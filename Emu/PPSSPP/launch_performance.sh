80b8c09152c5e29a32e9fd632080e80b3291f4d3
ninoh-fox <ninohfox@gmail.com>
2025-06-12 11:02:03 +0200

    add missing ext lists roms

--- Emu/PPSSPP/launch_performance.sh
+++ Emu/PPSSPP/launch_performance.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-echo 1 > /sys/devices/system/cpu/cpu0/online
-echo 1 > /sys/devices/system/cpu/cpu1/online
-echo 1 > /sys/devices/system/cpu/cpu2/online
-echo 1 > /sys/devices/system/cpu/cpu3/online
 echo $0 $*
 progdir=`dirname "$0"`
 cd $progdir
