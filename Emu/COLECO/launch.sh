ae7d84110a01bc52660f030c71a6041f29617507
ninoh-fox <ninohfox@gmail.com>
2025-06-13 10:40:29 +0200

    added colecovision

---
+++ Emu/COLECO/launch.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo $0 $*
+progdir=`dirname "$0"`
+export PATH="/mnt/SDCARD/miyoo355/bin:${PATH}"
+export LD_LIBRARY_PATH="/mnt/SDCARD/miyoo355/lib:${LD_LIBRARY_PATH}"
+
+RA_DIR=$progdir/../../RetroArch
+EMU_DIR=$progdir
+
+
+$EMU_DIR/cpufreq.sh
+
+
+cd $RA_DIR/
+
+#disable netplay
+NET_PARAM=
+
+HOME=$RA_DIR/ $RA_DIR/retroarch.flip -v $NET_PARAM -L $RA_DIR/.config/retroarch/cores/bluemsx_libretro.so "$*"
