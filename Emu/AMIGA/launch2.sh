36017bca2fdd14bdc99f2807083e29df2589e4f1
ninoh-fox <ninohfox@gmail.com>
2025-06-28 08:27:00 +0200

    added Amiga uae4arm core

---
+++ Emu/AMIGA/launch2.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "===================================="
+echo $0 $*
+progdir=`dirname "$0"`
+export PATH="/mnt/SDCARD/miyoo355/bin:${PATH}"
+export LD_LIBRARY_PATH="/mnt/SDCARD/miyoo355/lib:${LD_LIBRARY_PATH}"
+
+RA_DIR=$progdir/../../RetroArch
+EMU_DIR=$progdir
+
+cd $RA_DIR/
+
+$EMU_DIR/cpufreq.sh
+
+HOME=$RA_DIR/ $RA_DIR/retroarch.flip -v $NET_PARAM -L $RA_DIR/.config/retroarch/cores/uae4arm_libretro.so "$*"
